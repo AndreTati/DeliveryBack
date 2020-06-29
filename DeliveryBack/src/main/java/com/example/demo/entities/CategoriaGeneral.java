2b8fe76e116e486e7cd257c0ee3a98cc48b04d6a
AndreTati <andrea.duran@alumnos.frm.utn.edu.ar>
2020-06-29 02:12:00 -0300

    ultimas modificaciones

--- DeliveryBack/src/main/java/com/example/demo/entities/CategoriaGeneral.java
+++ DeliveryBack/src/main/java/com/example/demo/entities/CategoriaGeneral.java
@@ -1,10 +1,15 @@
 package com.example.demo.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.Where;
 
@@ -20,6 +25,8 @@ public class CategoriaGeneral {
 	private String denominacion;
 	@Column(name="eliminado_categoriaGeneral")
 	private boolean eliminado;
+	
+	
 	
 	public CategoriaGeneral() {}
 
@@ -46,6 +53,9 @@ public class CategoriaGeneral {
 	public void setEliminado(boolean eliminado) {
 		this.eliminado = eliminado;
 	}
+
+	
+	
 	
 	
 }
--- DeliveryBack/src/main/java/com/example/demo/repositories/ClienteRepository.java
+++ DeliveryBack/src/main/java/com/example/demo/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +17,7 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	@Transactional
 	@Query(value="UPDATE Persona SET eliminado_persona=true WHERE id_persona=?1", nativeQuery = true)
 	public int deleteById(int id);
+	
+	@Query(value = "SELECT * FROM Persona WHERE email_persona = ?1", nativeQuery  =true)
+	public Optional<Cliente> buscarPorEmail(String email);
 }
